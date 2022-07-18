b50dbfa54e092116c4007495142996f374b33e30
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-18 11:51:42 -0500

    Add placeholder class

---
+++ imageanalysis/ui/main_window.py
@@ -0,0 +1,15 @@
+"""
+Copyright (c) UChicago Argonne, LLC. All rights reserved.
+See LICENSE file.
+"""
+
+# ==================================================================================
+
+from pyqtgraph import QtGui
+
+# ==================================================================================
+
+class MainWindow(QtGui.QWidget):
+    
+    def __init__(self) -> None:
+        super(MainWindow, self).__init__()
