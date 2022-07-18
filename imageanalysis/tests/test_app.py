f46319d36dbf33eb74c3efffe71eadce582bc16b
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-18 12:09:34 -0500

    Add app creation test

--- imageanalysis/tests/test_app.py
+++ imageanalysis/tests/test_app.py
@@ -1,3 +1,9 @@
+from pyqtgraph import QtGui
 import pytest
 
 from imageanalysis import app
+
+# ==================================================================================
+
+def test_app_creation():
+    app.run()
