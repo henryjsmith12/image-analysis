767ad8b65aab109c1b497e95d47c83a45052249e
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-18 14:37:14 -0500

    Set basic window attributes

--- imageanalysis/ui/main_window.py
+++ imageanalysis/ui/main_window.py
@@ -13,3 +13,14 @@ class MainWindow(QtGui.QWidget):
     
     def __init__(self) -> None:
         super(MainWindow, self).__init__()
+
+        # Window attributes
+        self.setMinimumSize(1400, 800)
+        self.setGeometry(0, 50, 1400, 800)
+        self.setWindowTitle("image-analysis")
+
+        self.layout = QtGui.QGridLayout()
+
+        print(self.minimumSize())
+
+
