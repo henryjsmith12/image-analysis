7460effef0eec20d5242f3a0826d9ef7fdad4ba6
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-18 11:51:34 -0500

    Populate app creation file

---
+++ imageanalysis/app.py
@@ -0,0 +1,24 @@
+"""
+Copyright (c) UChicago Argonne, LLC. All rights reserved.
+See LICENSE file.
+"""
+
+# ==================================================================================
+
+from pyqtgraph import QtGui
+import sys
+
+from imageanalysis.ui.main_window import MainWindow
+
+# ==================================================================================
+
+def run():
+    app = QtGui.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec_()
+
+# ==================================================================================
+
+if __name__ == "__main__":
+    run()
