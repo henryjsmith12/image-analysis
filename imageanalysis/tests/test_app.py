a08c71837ff1fc370d3950e87a345d700abd1e08
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-18 14:21:54 -0500

    Remove test file

--- imageanalysis/tests/test_app.py
+++
@@ -1,9 +0,0 @@
-from pyqtgraph import QtGui
-import pytest
-
-from imageanalysis import app
-
-# ==================================================================================
-
-def test_app_creation():
-    app.run()
