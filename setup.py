0eb7a199280bb40c256f771c58bd2a59f7f1e0a5
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-18 11:08:13 -0500

    Add package setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+NAME = "Henry Smith"
+EMAIL = "smithh@anl.gov"
+URL = "https://github.com/henryjsmith12/image-analysis"
+VERSION = "0.0.1"
+DESCRIPTION = "Python-based software for XRD data analysis."
+LONG_DESCRIPTION = "Python-based software for XRD data analysis. Allows users to analyze SPEC data, raw XRD scan images, and reciprocal space-converted datasets."
+
+setup(
+    name="image-analysis", 
+    version=VERSION,
+    author=NAME,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    url=URL,
+    keywords=["python", "xrd"]
+)
