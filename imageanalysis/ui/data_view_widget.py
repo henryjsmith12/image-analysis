c5d0c5c9d455ae247f6aa822beb0c39e940d40e1
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-19 11:20:34 -0500

    Add data view class placeholder

---
+++ imageanalysis/ui/data_view_widget.py
@@ -0,0 +1,17 @@
+"""
+Copyright (c) UChicago Argonne, LLC. All rights reserved.
+See LICENSE file.
+"""
+
+# ==================================================================================
+
+from pyqtgraph import QtGui
+
+# ==================================================================================
+
+class DataViewWidget(QtGui.QWidget):
+    
+    def __init__(self) -> None:
+        super(DataViewWidget, self).__init__()
+
+# ==================================================================================
