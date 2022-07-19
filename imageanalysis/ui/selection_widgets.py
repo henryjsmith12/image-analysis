5ed823f6219b8f8a7e67b8063da27f896110ad48
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-19 11:17:37 -0500

    Add placeholder classes

---
+++ imageanalysis/ui/selection_widgets.py
@@ -0,0 +1,24 @@
+"""
+Copyright (c) UChicago Argonne, LLC. All rights reserved.
+See LICENSE file.
+"""
+
+# ==================================================================================
+
+from pyqtgraph import QtGui
+
+# ==================================================================================
+
+class ProjectSelectionWidget(QtGui.QWidget):
+
+    def __init__(self) -> None:
+        super(ProjectSelectionWidget, self).__init__()
+
+# ==================================================================================
+
+class ScanSelectionWidget(QtGui.QWidget):
+
+    def __init__(self) -> None:
+        super(ScanSelectionWidget, self).__init__()
+
+# ==================================================================================
