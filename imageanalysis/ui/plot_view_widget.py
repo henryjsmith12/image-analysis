f7e1114ea6bd737f8744250dbb4bdc363d0c7e8c
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-19 11:21:22 -0500

    Add plot view widget class placeholder

---
+++ imageanalysis/ui/plot_view_widget.py
@@ -0,0 +1,17 @@
+"""
+Copyright (c) UChicago Argonne, LLC. All rights reserved.
+See LICENSE file.
+"""
+
+# ==================================================================================
+
+from pyqtgraph import QtGui
+
+# ==================================================================================
+
+class PlotViewWidget(QtGui.QWidget):
+    
+    def __init__(self) -> None:
+        super(PlotViewWidget, self).__init__()
+
+# ==================================================================================
