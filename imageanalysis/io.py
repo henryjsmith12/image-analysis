08c44e3375fa70aa4433c6c32ae8f0bb7d68d72a
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-19 13:11:30 -0500

    Create isValidProjectPath function

--- imageanalysis/io.py
+++ imageanalysis/io.py
@@ -4,3 +4,38 @@ See LICENSE file.
 """
 
 # ==================================================================================
+
+import os
+from xml.etree import ElementTree
+
+# ==================================================================================
+
+def isValidProjectPath(path):
+    """
+    Check if a path has the correct structure to be a project path. A valid project 
+    directory includes: A SPEC file (.spec), instrument and detector configuration 
+    files (.xml), and an "images" subdirectory.
+    """
+
+    spec, instr_config, det_config, images = False, False, False, False
+
+    for item in os.listdir(path):
+        item_path = f"{path}/{item}"
+        if os.path.isdir(item_path):
+            if item == "images":
+                images = True
+        elif os.path.isfile(item_path):
+            if item.endswith(".spec"):
+                spec = True
+            elif item.endswith(".xml"):
+                if not instr_config:
+                    instr_config = True
+                else:
+                    det_config = True
+        is_valid = not False in [spec, instr_config, det_config, images]
+        if is_valid:
+            break
+
+    return is_valid
+    
+# ==================================================================================
