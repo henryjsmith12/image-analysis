c614fc20e7e41a34dfdbe86b1e6bda0f6f3cefff
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-21 12:30:17 -0500

    Add path variables to Project constructor

--- imageanalysis/structures.py
+++ imageanalysis/structures.py
@@ -5,7 +5,8 @@ See LICENSE file.
 
 # ==================================================================================
 
-
+import spec2nexus as s2n
+from spec2nexus import spec
 
 # ==================================================================================
 
@@ -18,7 +19,15 @@ class Project:
         instrument_path: str,
         detector_path: str
     ) -> None:
-        pass
+        
+        # Path variables
+        self.path = project_path
+        self.spec_path = spec_path
+        self.instrument_path = instrument_path
+        self.detector_path = detector_path
+
+        self.spec_data = spec.SpecDataFile(spec_path)
+        self.scan_numbers = self.spec_data.getScanNumbers()
 
 # ==================================================================================
 
