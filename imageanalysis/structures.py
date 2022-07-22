6e3e36523ae2d597f3929aad99c4acbf7c442ace
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-22 15:17:15 -0500

    Add constructor for Scan class

--- imageanalysis/structures.py
+++ imageanalysis/structures.py
@@ -5,6 +5,10 @@ See LICENSE file.
 
 # ==================================================================================
 
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
 import spec2nexus as s2n
 from spec2nexus import spec
 
@@ -33,11 +37,51 @@ class Project:
         self.spec_data = spec.SpecDataFile(spec_path)
         self.scan_numbers = self.spec_data.getScanNumbers()
 
+        self.scans = [Scan(self.spec_data.getScan(i), project_path, spec_path) for i in self.scan_numbers]
+
 # ==================================================================================
 
 class Scan:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, spec_scan, project_path, spec_path) -> None:
+
+        self.spec_data = None
+        self.raw_image_data = None
+        self.reciprocal_space_map = None
+        self.gridded_image_data, self.gridded_image_coords = None, None
+
+        self.spec_scan = spec_scan
+        self.number = spec_scan.scanNum
+        self.project_path = project_path
+        self.spec_path = spec_path
+        self.raw_image_path = f"{project_path}/images/{os.path.basename(os.path.splitext(self.spec_path)[0])}/S{str(self.number).zfill(3)}"
+
+        self.spec_data = spec_scan.data
+        self.raw_image_data = self.getImageData()
+        self.reciprocal_space_map = self.mapImageData()
+
+    # ------------------------------------------------------------------------------
+
+    def getImageData(self):
+        image_paths = sorted(os.listdir(self.raw_image_path))
+        image_data = []
+
+        for img_basepath in image_paths:
+            img_path = f"{self.raw_image_path}/{img_basepath}"
+            img_array = plt.imread(img_path)
+            image_data.append(img_array)
+
+        image_data = np.array(image_data)
+        return image_data
+
+    # ------------------------------------------------------------------------------
+
+    def mapImageData(self):
+        ...
+
+    # ------------------------------------------------------------------------------
+
+    def gridImageData(self):
+        ...
 
 # ==================================================================================
