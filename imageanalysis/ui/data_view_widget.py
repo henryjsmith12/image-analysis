cbee90fd33a122420e12631944caf39923716da6
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-23 19:30:40 -0500

    Add tab widget

--- imageanalysis/ui/data_view_widget.py
+++ imageanalysis/ui/data_view_widget.py
@@ -14,4 +14,9 @@ class DataViewWidget(QtGui.QWidget):
     def __init__(self) -> None:
         super(DataViewWidget, self).__init__()
 
+        self.tab_widget = QtGui.QTabWidget()
+        self.layout = QtGui.QGridLayout()
+        self.setLayout(self.layout)
+        self.layout.addWidget(self.tab_widget)
+
 # ==================================================================================
