b7f6e5002ab5f72b2129e43966d90cfe33137105
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-23 19:52:21 -0500

    Add DataViewTab class

--- imageanalysis/ui/data_view_widget/__init__.py
+++ imageanalysis/ui/data_view_widget/__init__.py
@@ -9,12 +9,22 @@ from pyqtgraph import QtGui
 
 # ==================================================================================
 
-class DataViewWidget(QtGui.QWidget):
+class DataViewWidget(QtGui.QTabWidget):
     
     def __init__(self) -> None:
         super(DataViewWidget, self).__init__()
 
+# ==================================================================================
+
+class DataViewTab(QtGui.QWidget):
+
+    def __init__(self) -> None:
+        super(DataViewTab, self).__init__()
+
         self.tab_widget = QtGui.QTabWidget()
+        self.tab_widget.addTab(QtGui.QWidget(), "SPEC")
+        self.tab_widget.addTab(QtGui.QWidget(), "Raw")
+        self.tab_widget.addTab(QtGui.QWidget(), "Gridded")
         self.layout = QtGui.QGridLayout()
         self.setLayout(self.layout)
         self.layout.addWidget(self.tab_widget)
