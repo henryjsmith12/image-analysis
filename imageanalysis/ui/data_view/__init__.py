5b337164cf5bf835da9fc15c7dde20d4114769eb
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-07-29 14:30:39 -0500

    Add new local ui dependencies

--- imageanalysis/ui/data_view/__init__.py
+++ imageanalysis/ui/data_view/__init__.py
@@ -7,12 +7,20 @@ See LICENSE file.
 
 from pyqtgraph import QtGui
 
+# ----------------------------------------------------------------------------------
+
+from imageanalysis.ui.data_view.gridded_data import GriddedDataWidget
+from imageanalysis.ui.data_view.raw_data import RawDataWidget
+from imageanalysis.ui.data_view.spec_data import SpecDataWidget
+
 # ==================================================================================
 
 class DataView(QtGui.QTabWidget):
     
     def __init__(self) -> None:
         super(DataView, self).__init__()
+
+        self.addTab(DataViewTab(), "840")
 
 # ==================================================================================
 
@@ -22,9 +30,9 @@ class DataViewTab(QtGui.QWidget):
         super(DataViewTab, self).__init__()
 
         self.tab_widget = QtGui.QTabWidget()
-        self.tab_widget.addTab(QtGui.QWidget(), "SPEC")
-        self.tab_widget.addTab(QtGui.QWidget(), "Raw")
-        self.tab_widget.addTab(QtGui.QWidget(), "Gridded")
+        #self.tab_widget.addTab(SpecDataWidget(), "SPEC")
+        self.tab_widget.addTab(RawDataWidget(), "Raw")
+        self.tab_widget.addTab(GriddedDataWidget(), "Gridded")
         self.layout = QtGui.QGridLayout()
         self.setLayout(self.layout)
         self.layout.addWidget(self.tab_widget)
