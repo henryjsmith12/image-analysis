ebe5b2e4d8a329eceb2d79b666666a0cb1b84d47
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-08-02 13:38:50 -0500

    Connect controller to ImageTool

--- imageanalysis/ui/data_view/gridded_data.py
+++ imageanalysis/ui/data_view/gridded_data.py
@@ -5,10 +5,8 @@ See LICENSE file.
 
 # ==================================================================================
 
-from matplotlib.colors import LogNorm
 import numpy as np
-import pyqtgraph as pg
-from pyqtgraph import QtGui
+from pyqtgraph import QtCore, QtGui
 from pyqtgraph.dockarea import Dock, DockArea
 
 # ----------------------------------------------------------------------------------
@@ -24,10 +22,10 @@ class GriddedDataWidget(DockArea):
 
         self.scan = scan
 
-        self.controller = GriddedDataController()
         self.image_tool_3d = ImageTool()
         self.image_tool_2d = ImageTool()
-
+        self.controller = GriddedDataController(scan, self.image_tool_3d)
+        
         self.controller_dock = Dock(
             name="Controller",
             size=(1, 1),
@@ -49,19 +47,163 @@ class GriddedDataWidget(DockArea):
             hideTitle=True,
             closable=False
         )
-        self.controller_dock.setMaximumHeight(125)
+        self.controller_dock.setMaximumHeight(200)
         self.addDock(self.controller_dock)
         self.addDock(self.image_tool_3d_dock, "bottom" ,self.controller_dock)
-        self.addDock(self.image_tool_2d_dock, "bottom" ,self.image_tool_3d_dock)
+        #self.addDock(self.image_tool_2d_dock, "bottom" ,self.image_tool_3d_dock)
         
 # ==================================================================================
 
 class GriddedDataController(QtGui.QWidget):
 
-    def __init__(self) -> None:
+    orderUpdated = QtCore.pyqtSignal()
+
+    def __init__(self, scan, image_tool) -> None:
         super(GriddedDataController, self).__init__()
 
+        self.data = scan.gridded_image_data
+        self.dim_order = (0, 1, 2)
+        self.image_tool = image_tool
+        self.index = None
+        self.scan = scan
+        h_info = {"label" : "H", "coords" : self.scan.gridded_image_coords[0]}
+        k_info = {"label" : "K", "coords" : self.scan.gridded_image_coords[1]}
+        l_info = {"label" : "L", "coords" : self.scan.gridded_image_coords[2]}
+        self.setAcceptDrops(True)
+
+        # Child widgets
+        self.h_controller = GriddedDimensionController(self, h_info)
+        self.h_controller.setEnabled(False)
+        self.k_controller = GriddedDimensionController(self, k_info)
+        self.k_controller.setEnabled(False)
+        self.l_controller = GriddedDimensionController(self, l_info)
+        self.dimension_controllers = [self.h_controller, self.k_controller, self.l_controller]
+
+        # Layout
+        self.layout = QtGui.QVBoxLayout()
+        self.setLayout(self.layout)
+        for dim_ctrl in self.dimension_controllers:
+            self.layout.addWidget(dim_ctrl)
+
+        # Connections
+        for dim_ctrl in self.dimension_controllers:
+            dim_ctrl.updated.connect(self.setIndex)
+            dim_ctrl.updated.connect(self.setImage)
+        self.orderUpdated.connect(self.setDimensionOrder)
+        self.orderUpdated.connect(self.setImage)
+
+    # ------------------------------------------------------------------------------
+
+    def setDimensionOrder(self):
+        dim_order = []
+        for i in range(self.layout.count()):
+            dim_ctrl = self.layout.itemAt(i).widget()
+            dim = [self.h_controller, self.k_controller, self.l_controller].index(dim_ctrl)
+            dim_order.append(dim)
+        self.dim_order = tuple(dim_order)
+
+        self.layout.itemAt(0).widget().setEnabled(False)
+        self.layout.itemAt(1).widget().setEnabled(False)
+        self.layout.itemAt(2).widget().setEnabled(True)
+
+    # ------------------------------------------------------------------------------
+
+    def setIndex(self):
+        sender = self.sender()
+        self.index = sender.index
+
+    # ------------------------------------------------------------------------------
+
+    def setImage(self):
+        if self.data is None:
+            self.data = self.scan.gridded_image_data
+
+        index = self.index
+        dim_order = self.dim_order # e.g. (0, 1, 2)
+        data = self.data.transpose(*dim_order)
+        labels = [x for i, x in sorted(zip(list(dim_order), ["H", "K", "L"]))]
+        image = data[:, :, index]
+
+        self.image_tool.setImage(self.data, image, labels[0], labels[1])
+
+    # ------------------------------------------------------------------------------
+
+    def dragEnterEvent(self, e):
+        e.accept()
+
+    # ------------------------------------------------------------------------------
+
+    def dropEvent(self, e):
+        drop_pos = e.pos()
+        widget = e.source()
+
+        for i in range(self.layout.count()):
+            w = self.layout.itemAt(i).widget()
+            if drop_pos.y() < w.y():
+                self.layout.insertWidget(i - 1, widget)
+                break
+            elif i == self.layout.count() - 1:
+                self.layout.insertWidget(i, widget)
+                break
+
+        e.accept()
+        self.orderUpdated.emit()
+    
+# ==================================================================================
+
+class GriddedDimensionController(QtGui.QGroupBox):
+
+    updated = QtCore.pyqtSignal()
+
+    def __init__(self, parent, dim_info) -> None:
+        super(GriddedDimensionController, self).__init__()
+
+        self.parent = parent
+        self.index = 0
+
+        # Child widgets
+        self.dim_lbl = QtGui.QLabel(dim_info["label"])
+        self.dim_slider = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.dim_slider.setMaximum(len(dim_info["coords"]) - 1)
+        self.dim_cbx = QtGui.QComboBox()
+        coords = [str(round(i, 5)) for i in dim_info["coords"]]
+        self.dim_cbx.addItems(coords)
+
+        # Layout
         self.layout = QtGui.QGridLayout()
         self.setLayout(self.layout)
+        self.layout.addWidget(self.dim_lbl, 0, 0)
+        self.layout.addWidget(self.dim_slider, 0, 1, 1, 5)
+        self.layout.addWidget(self.dim_cbx, 0, 6, 1, 1)
+        for i in range(self.layout.columnCount()):
+            self.layout.setRowStretch(i, 1)
+
+        # Connections
+        self.dim_slider.valueChanged.connect(self.setIndex)
+        self.dim_cbx.currentIndexChanged.connect(self.setIndex)
+
+    # ------------------------------------------------------------------------------
+
+    def setIndex(self):
+        sender = self.sender()
+        index = None
+        if sender == self.dim_slider:
+            index = sender.value()
+            self.dim_cbx.setCurrentIndex(index)
+        elif sender == self.dim_cbx:
+            index = sender.currentIndex()
+            self.dim_slider.setValue(index)
+        self.index = index
+        self.updated.emit()
+
+    # ------------------------------------------------------------------------------
+    
+    def mouseMoveEvent(self, e):
+        if isinstance(self.parent, GriddedDataController):
+            if e.buttons() == QtCore.Qt.LeftButton:
+                drag = QtGui.QDrag(self)
+                mime = QtCore.QMimeData()
+                drag.setMimeData(mime)
+                drag.exec_(QtCore.Qt.MoveAction)
 
 # ==================================================================================
