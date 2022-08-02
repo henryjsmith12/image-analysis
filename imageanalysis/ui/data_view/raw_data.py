21c39c7d2c79f1a9e9231d98c640b7c6e90419ec
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-08-02 13:39:01 -0500

    Connect controller to ImageTool

--- imageanalysis/ui/data_view/raw_data.py
+++ imageanalysis/ui/data_view/raw_data.py
@@ -5,10 +5,6 @@ See LICENSE file.
 
 # ==================================================================================
 
-from matplotlib.colors import LogNorm, Normalize
-import matplotlib.pyplot as plt
-import numpy as np
-import pyqtgraph as pg
 from pyqtgraph import QtCore, QtGui
 from pyqtgraph.dockarea import Dock, DockArea
 
@@ -25,9 +21,9 @@ class RawDataWidget(DockArea):
 
         self.scan = scan
 
-        self.controller = RawDataController()
         self.image_tool_3d = ImageTool()
         self.image_tool_2d = ImageTool()
+        self.controller = RawDataController(scan, self.image_tool_3d)
 
         self.controller_dock = Dock(
             name="Controller",
@@ -53,25 +49,34 @@ class RawDataWidget(DockArea):
         self.controller_dock.setMaximumHeight(75)
         self.addDock(self.controller_dock)
         self.addDock(self.image_tool_3d_dock, "bottom" ,self.controller_dock)
-        self.addDock(self.image_tool_2d_dock, "bottom" ,self.image_tool_3d_dock)
+        #self.addDock(self.image_tool_2d_dock, "bottom" ,self.image_tool_3d_dock)
 
 # ==================================================================================
 
 class RawDataController(QtGui.QWidget):
 
-    def __init__(self) -> None:
+    def __init__(self, scan, image_tool) -> None:
         super(RawDataController, self).__init__()
 
+        self.data = scan.raw_image_data
+        self.scan = scan
+        self.image_tool = image_tool
+
+        # Child widgets
         self.data_slider = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.data_slider.setMaximum(self.data.shape[0] - 1)
         self.data_sbx = QtGui.QSpinBox()
+        self.data_sbx.setMaximum(self.data.shape[0] - 1)
         self.data_type_rbg = QtGui.QButtonGroup()
         self.h_rbtn = QtGui.QRadioButton("H")
         self.k_rbtn = QtGui.QRadioButton("K")
         self.l_rbtn = QtGui.QRadioButton("L")
         self.intensity_rbtn = QtGui.QRadioButton("Intensity")
+        self.intensity_rbtn.setChecked(True)
         for btn in [self.h_rbtn, self.k_rbtn, self.l_rbtn, self.intensity_rbtn]:
             self.data_type_rbg.addButton(btn)
 
+        # Layout
         self.layout = QtGui.QGridLayout()
         self.setLayout(self.layout)
         self.layout.addWidget(self.data_slider, 0, 0, 1, 3)
@@ -84,5 +89,52 @@ class RawDataController(QtGui.QWidget):
             self.layout.setRowStretch(i, 1)
         for i in range(self.layout.columnCount()):
             self.layout.setRowStretch(i, 1)
+
+        # Connections
+        for btn in [self.h_rbtn, self.k_rbtn, self.l_rbtn, self.intensity_rbtn]:
+            btn.toggled.connect(self.setDataType)
+            btn.toggled.connect(self.setImage)
+        self.data_slider.valueChanged.connect(self.setIndex)
+        self.data_slider.valueChanged.connect(self.setImage)
+        self.data_sbx.valueChanged.connect(self.setIndex)
+        self.data_sbx.valueChanged.connect(self.setImage)
+
+        # Display first image
+        self.setImage()
+
+    # ------------------------------------------------------------------------------
+
+    def setDataType(self):
+        btn = self.sender()
+        if btn.isChecked():
+            if btn.text() == "Intensity":
+                self.data = self.scan.raw_image_data
+            elif btn.text() == "H":
+                self.data = self.scan.h_map
+            elif btn.text() == "K":
+                self.data = self.scan.k_map
+            elif btn.text() == "L":
+                self.data = self.scan.l_map
+
+            self.data_slider.setMaximum(self.data.shape[0] - 1)
+            self.data_sbx.setMaximum(self.data.shape[0] - 1)
+
+    # ------------------------------------------------------------------------------
+
+    def setIndex(self):
+        sender = self.sender()
+        index = sender.value()
+        if sender == self.data_slider:
+            self.data_sbx.setValue(index)
+        elif sender == self.data_sbx:
+            self.data_slider.setValue(index)
+
+    # ------------------------------------------------------------------------------
+
+    def setImage(self):
+        index = self.data_sbx.value()
+        image = self.data[index]
+
+        self.image_tool.setImage(self.data, image)
 
 # ==================================================================================
