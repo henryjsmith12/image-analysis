7d3204a0c7542366beeff4e0c6e236ae5614ef15
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-08-12 13:34:31 -0500

    Edit formatting to be PEP-8 compliant

--- imageanalysis.py
+++ imageanalysis.py
@@ -3,16 +3,12 @@ Copyright (c) UChicago Argonne, LLC. All rights reserved.
 See LICENSE file.
 """
 
-# ==================================================================================
 
 import sys
+
 from imageanalysis import app
 
-# ==================================================================================
 
 # Repository driver code
 if __name__ == '__main__':
     sys.exit(app.run())
-
-# ==================================================================================
-
