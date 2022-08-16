8d62f262c2de8b668e81b77b029dd56027c004b0
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-08-16 08:57:41 -0500

    v0.0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 NAME = "Henry Smith"
 EMAIL = "smithh@anl.gov"
 URL = "https://github.com/henryjsmith12/image-analysis"
-VERSION = "0.0.5"
+VERSION = "0.0.8"
 DESCRIPTION = "Python-based software for XRD data analysis."
 LONG_DESCRIPTION = "Python-based software for XRD data analysis. Allows users to analyze raw XRD scan images and reciprocal space-converted datasets."
 
@@ -15,7 +15,16 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "matplotlib==3.5.3",
+        "numpy==1.23.1",
+        "PyQt5==5.15.7",
+        "pyqtgraph==0.12.1",
+        "rsMap3D==1.2.1",
+        "spec2nexus==2021.1.3",
+        "tifffile==2022.8.8",
+        "xrayutilities==1.7.3"
+    ],
     url=URL,
     keywords=["python", "pyqtgraph", "image-analysis", "xrd"]
 )
