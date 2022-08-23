14c93cddf48394e2135363ad57ac9182b07caa38
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-08-23 14:30:37 -0500

    Add Project args tests

---
+++ test/test_structures.py
@@ -0,0 +1,129 @@
+import pytest
+
+from imageanalysis.structures import Project, Scan
+
+def test_project_creation_success():
+    p = Project(
+        project_path="sample_project/",
+        spec_path="sample_project/pmn_pt011_2_1.spec",
+        instrument_path="sample_project/6IDB_Instrument.xml",
+        detector_path="sample_project/6IDB_DetectorGeometry.xml"
+    )
+
+
+def test_project_creation_empty_project_path():
+    with pytest.raises(ValueError) as ex_info:
+        p = Project(
+            project_path=None,
+            spec_path="sample_project/pmn_pt011_2_1.spec",
+            instrument_path="sample_project/6IDB_Instrument.xml",
+            detector_path="sample_project/6IDB_DetectorGeometry.xml"
+        )
+
+
+def test_project_creation_empty_spec_path():
+    with pytest.raises(ValueError) as ex_info:
+        p = Project(
+            project_path="sample_project/",
+            spec_path=None,
+            instrument_path="sample_project/6IDB_Instrument.xml",
+            detector_path="sample_project/6IDB_DetectorGeometry.xml"
+        )
+
+
+def test_project_creation_empty_instrument_path():
+    with pytest.raises(ValueError) as ex_info:
+        p = Project(
+            project_path="sample_project/",
+            spec_path="sample_project/pmn_pt011_2_1.spec",
+            instrument_path=None,
+            detector_path="sample_project/6IDB_DetectorGeometry.xml"
+        )
+
+
+def test_project_creation_empty_detector_path():
+    with pytest.raises(ValueError) as ex_info:
+        p = Project(
+            project_path="sample_project/",
+            spec_path="sample_project/pmn_pt011_2_1.spec",
+            instrument_path="sample_project/6IDB_Instrument.xml",
+            detector_path=None
+        )
+
+def test_project_creation_nonstring_project_path():
+    with pytest.raises(ValueError) as ex_info:
+        p = Project(
+            project_path=42,
+            spec_path="sample_project/pmn_pt011_2_1.spec",
+            instrument_path="sample_project/6IDB_Instrument.xml",
+            detector_path="sample_project/6IDB_DetectorGeometry.xml"
+        )
+
+
+def test_project_creation_nonstring_spec_path():
+    with pytest.raises(ValueError) as ex_info:
+        p = Project(
+            project_path="sample_project/",
+            spec_path=42,
+            instrument_path="sample_project/6IDB_Instrument.xml",
+            detector_path="sample_project/6IDB_DetectorGeometry.xml"
+        )
+
+
+def test_project_creation_nonstring_instrument_path():
+    with pytest.raises(ValueError) as ex_info:
+        p = Project(
+            project_path="sample_project/",
+            spec_path="sample_project/pmn_pt011_2_1.spec",
+            instrument_path=42,
+            detector_path="sample_project/6IDB_DetectorGeometry.xml"
+        )
+
+
+def test_project_creation_nonstring_detector_path():
+    with pytest.raises(ValueError) as ex_info:
+        p = Project(
+            project_path="sample_project/",
+            spec_path="sample_project/pmn_pt011_2_1.spec",
+            instrument_path="sample_project/6IDB_Instrument.xml",
+            detector_path=42
+        )
+
+def test_project_creation_nonstring_project_path():
+    with pytest.raises(NotADirectoryError) as ex_info:
+        p = Project(
+            project_path="INVALID_PATH_STRING",
+            spec_path="sample_project/pmn_pt011_2_1.spec",
+            instrument_path="sample_project/6IDB_Instrument.xml",
+            detector_path="sample_project/6IDB_DetectorGeometry.xml"
+        )
+
+
+def test_project_creation_nonstring_spec_path():
+    with pytest.raises(FileNotFoundError) as ex_info:
+        p = Project(
+            project_path="sample_project/",
+            spec_path="INVALID_PATH_STRING",
+            instrument_path="sample_project/6IDB_Instrument.xml",
+            detector_path="sample_project/6IDB_DetectorGeometry.xml"
+        )
+
+
+def test_project_creation_nonstring_instrument_path():
+    with pytest.raises(FileNotFoundError) as ex_info:
+        p = Project(
+            project_path="sample_project/",
+            spec_path="sample_project/pmn_pt011_2_1.spec",
+            instrument_path="INVALID_PATH_STRING",
+            detector_path="sample_project/6IDB_DetectorGeometry.xml"
+        )
+
+
+def test_project_creation_nonstring_detector_path():
+    with pytest.raises(FileNotFoundError) as ex_info:
+        p = Project(
+            project_path="sample_project/",
+            spec_path="sample_project/pmn_pt011_2_1.spec",
+            instrument_path="sample_project/6IDB_Instrument.xml",
+            detector_path="INVALID_PATH_STRING"
+        )
