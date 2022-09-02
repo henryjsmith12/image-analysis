8b94236be3c0c2e6c2565f432ecf14defb911d46
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-09-02 07:56:48 -0500

    Add basic ImageTool mouse/cmap functions

--- imageanalysis/ui/data_view/image_tool/__init__.py
+++ imageanalysis/ui/data_view/image_tool/__init__.py
@@ -1,12 +1,19 @@
 import numpy as np
 import pyqtgraph as pg
-from pyqtgraph import QtGui
+from pyqtgraph import QtCore, QtGui
 from pyqtgraph.dockarea import Dock, DockArea
+
+from imageanalysis.ui.data_view.image_tool.color_mapping import ColorMapController
 
 class ImageTool(DockArea):
 
-    def __init__(self) -> None:
+    def __init__(self, parent) -> None:
         super().__init__()
+
+        self.parent = parent
+
+        self.data = None
+        self.data_min, self.data_max = None, None
 
         self.plot_3d = ImagePlot(parent=self)
         self.plot_2d = ImagePlot(parent=self)
@@ -45,11 +52,12 @@ class ImageTool(DockArea):
         self.addDock(self.plot_3d_dock, "left", self.controller_dock)
         self.addDock(self.plot_2d_dock, "bottom", self.plot_3d_dock)
         self.addDock(self.plot_1d_dock, "bottom", self.plot_2d_dock)
+        self.controller_dock.setMaximumWidth(200)
+        self.controller_dock.setMinimumWidth(200)
 
         # If ROI is activated, proper dock will show
-        '''self.plot_2d_dock.hide()
-        self.plot_1d_dock.hide()'''
-
+        self.plot_2d_dock.hide()
+        self.plot_1d_dock.hide()
 
     def _setImage(
         self,
@@ -60,11 +68,44 @@ class ImageTool(DockArea):
         x_coords: list=None,
         y_coords: list=None
     ):
-        self.data = data
-        self.plot_3d._setLabels(x=x_label, y=y_label)
-        self.plot_3d._setImage(image=image)
-        self.plot_3d._setCoordinates(x=x_coords, y=y_coords)
+        if self.data is None:
+            self.data = data
+            self.data_min, self.data_max = np.amin(data), np.amax(data)
+        
+            self.plot_3d._setLabels(x=x_label, y=y_label)
+            self.plot_3d._setImage(image=image)
+            self.plot_3d._setCoordinates(x=x_coords, y=y_coords)
 
+            self.controller._setColorMap()
+        else:
+            self.plot_3d._setLabels(x=x_label, y=y_label)
+            self.plot_3d._setImage(image=image)
+            self.plot_3d._setCoordinates(x=x_coords, y=y_coords)
+
+    def _setColorMap(self, color_map):
+        self.plot_3d.setColorMap(color_map)
+        
+        color_bar_3d = pg.ColorBarItem(
+            values=(self.data_min, self.data_max),
+            cmap=color_map,
+            interactive=False,
+            width=15,
+            orientation="h"
+        )
+        
+        color_bar_3d.setImageItem(
+            img=self.plot_3d.image,
+            insert_in=self.plot_3d.getView()
+        )
+        
+    def _getMouseCoordinates(self, x, y):
+        if x is not None:
+            h, k, l = None, None, None
+            print(x, y)
+        else:
+            h, k, l = None, None, None
+        #self.controller._setMouseInfo(h=h, k=k, l=l, value=value)
+        ...
 
 class ImageToolController(QtGui.QWidget):
 
@@ -73,13 +114,30 @@ class ImageToolController(QtGui.QWidget):
 
         self.image_tool = parent
 
-        # Color Map
-        # Mouse
-        # Options for 2 Image Plots
-        # Options for Line Plot
+        self.mouse_info_widget = MouseInfoWidget()
+        self.color_map_ctrl = ColorMapController()
+        self.plot_3d_ctrl = QtGui.QGroupBox()
+        self.plot_2d_ctrl = QtGui.QGroupBox()
+        self.plot_1d_ctrl = QtGui.QGroupBox()
 
-        self.layout = QtGui.QGridLayout()
+        self.layout = QtGui.QVBoxLayout()
         self.setLayout(self.layout)
+
+        self.layout.addWidget(self.mouse_info_widget)
+        self.layout.addWidget(self.color_map_ctrl)
+        self.layout.addWidget(self.plot_3d_ctrl)
+        self.layout.addWidget(self.plot_2d_ctrl)
+        self.layout.addWidget(self.plot_1d_ctrl)
+
+        self.color_map_ctrl.colorMapChanged.connect(self._setColorMap)
+
+    def _setMouseInfo(self):
+        # Calls mouse info
+        ...
+
+    def _setColorMap(self):
+        color_map = self.color_map_ctrl.color_map
+        self.image_tool._setColorMap(color_map=color_map)
 
 class ImagePlot(pg.ImageView):
 
@@ -92,12 +150,15 @@ class ImagePlot(pg.ImageView):
         self.image_tool = parent
         self.image = None
         self.transform = None
+        self.x_coords, self.y_coords = None, None
 
         self.ui.histogram.hide()
         self.ui.roiBtn.hide()
         self.ui.menuBtn.hide()
         self.getView().setAspectLocked(False)
         self.getView().ctrlMenu = None
+
+        self.getView().scene().sigMouseMoved.connect(self._getMouseInfo)
 
     def _setImage(self, image: np.ndarray):
         self.image = image
@@ -138,8 +199,20 @@ class ImagePlot(pg.ImageView):
             pos = (x[0], y[0])
             self.transform.translate(*pos)
             self.transform.scale(*scale)
+            self.x_coords, self.y_coords = x, y
         else:
             self.transform = None
+
+    def _getMouseInfo(self, scene_point=None):
+        
+        if scene_point is not None:
+            view_point = self.getView().vb.mapSceneToView(scene_point)
+            x = int(self.image.shape[0] * ((view_point.x() - self.x_coords[0]) / (self.x_coords[-1] - self.x_coords[0])))
+            y = int(self.image.shape[1] * ((view_point.y() - self.y_coords[0]) / (self.y_coords[-1] - self.y_coords[0])))
+            if 0 <= x < self.image.shape[0] and 0 <= y < self.image.shape[1]:
+                self.image_tool._getMouseCoordinates(x, y)
+            else:
+                self.image_tool._getMouseCoordinates(None, None)
 
 
 class LinePlot(pg.PlotWidget):
@@ -154,3 +227,49 @@ class LinePlot(pg.PlotWidget):
 
     def _setLabels():
         ...
+
+class MouseInfoWidget(QtGui.QGroupBox):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.h_lbl, self.h_txt = QtGui.QLabel("H: "), QtGui.QLineEdit()
+        self.h_lbl.setAlignment(
+            QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter
+        )
+        self.k_lbl, self.k_txt = QtGui.QLabel("K: "), QtGui.QLineEdit()
+        self.k_lbl.setAlignment(
+            QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter
+        )
+        self.l_lbl, self.l_txt = QtGui.QLabel("L: "), QtGui.QLineEdit()
+        self.l_lbl.setAlignment(
+            QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter
+        )
+        self.value_lbl = QtGui.QLabel("Value: ")
+        self.value_lbl.setAlignment(
+            QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter
+        )
+        self.value_txt = QtGui.QLineEdit()
+
+        self.layout = QtGui.QGridLayout()
+        self.setLayout(self.layout)
+        self.layout.addWidget(self.h_lbl, 0, 0)
+        self.layout.addWidget(self.h_txt, 0, 1, 1, 5)
+        self.layout.addWidget(self.k_lbl, 1, 0)
+        self.layout.addWidget(self.k_txt, 1, 1, 1, 5)
+        self.layout.addWidget(self.l_lbl, 2, 0)
+        self.layout.addWidget(self.l_txt, 2, 1, 1, 5)
+        self.layout.addWidget(self.value_lbl, 3, 0)
+        self.layout.addWidget(self.value_txt, 3, 1, 1, 5)
+        self.layout.setColumnStretch(0, 1)
+        self.layout.setColumnStretch(1, 1)
+        self.layout.setColumnStretch(2, 1)
+        self.layout.setColumnStretch(3, 1)
+        self.layout.setColumnStretch(4, 1)
+        self.layout.setColumnStretch(5, 1)
+
+    def _updateMouseInfo(self, h=None, k=None, l=None, value=None):
+        self.h_txt.setText(str(round(h, 5) or ""))
+        self.k_txt.setText(str(round(k, 5) or ""))
+        self.l_txt.setText(str(round(l, 5) or ""))
+        self.value_txt.setText(str(value or ""))
