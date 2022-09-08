08ba3e3ba6a16035c1df66c533f60f69605c1438
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-09-08 11:06:45 -0500

    Add placeholder class for LineSegmentROI

--- imageanalysis/ui/data_view/image_tool/roi.py
+++ imageanalysis/ui/data_view/image_tool/roi.py
@@ -1,6 +1,24 @@
+import pyqtgraph as pg
 from pyqtgraph import QtGui
+
+from imageanalysis.ui.data_view.image_tool import ImagePlot
 
 class ROIController(QtGui.QWidget):
 
     def __init__(self) -> None:
         super().__init__()
+
+class LineSegmentROI(pg.LineSegmentROI):
+
+    def __init__(
+        self,
+        parent_plot: ImagePlot,
+        child_plot
+    ) -> None:
+        super().__init__()
+
+    def _getSlice(self) -> None:
+        ...
+
+    def _setImage(self) -> None:
+        ...
