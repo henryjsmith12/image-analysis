03866b9c44325a12cc19795552c26d5fe40de662
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-10-20 11:29:13 -0500

    Add skeleton for PlotArea

--- imageanalysis/ui/plot_view/__init__.py
+++ imageanalysis/ui/plot_view/__init__.py
@@ -1,17 +1,28 @@
-"""
-Copyright (c) UChicago Argonne, LLC. All rights reserved.
+"""Copyright (c) UChicago Argonne, LLC. All rights reserved.
+
 See LICENSE file.
 """
 
-# ==================================================================================
 
-from pyqtgraph import QtGui
+import pyqtgraph as pg
+from pyqtgraph import QtCore, QtGui
+from pyqtgraph.dockarea import Dock, DockArea
 
-# ==================================================================================
+from imageanalysis.structures import Scan
+from imageanalysis.ui.data_view.gridded_data import GriddedDataWidget
+from imageanalysis.ui.data_view.raw_data import RawDataWidget
 
-class PlotView(QtGui.QWidget):
-    
+
+class PlotView(DockArea):
+    """Houses a tab widget for DataViewTab objects."""
+
     def __init__(self) -> None:
         super(PlotView, self).__init__()
 
-# ==================================================================================
+class PlotArea(pg.PlotWidget):
+    ...
+
+class PlotDetailsWidget(QtGui.QWidget):
+    ...
+
+    
